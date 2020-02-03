7c6264dac1cf395e93e4ca357cca54022e31ff7b
loloara <addy94@naver.com>
2020-02-03 14:19:24 +0900

    first commit

---
+++ genreisromance-backend/src/test/java/com/loloara/genreisromance/GenreisromanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loloara.genreisromance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenreisromanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
