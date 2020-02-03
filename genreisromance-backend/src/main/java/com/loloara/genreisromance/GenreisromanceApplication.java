2f542a0e56abbd39b67bb824d392f4d11068bd95
loloara <addy94@naver.com>
2020-02-03 15:56:52 +0900

    set default profile for project

--- genreisromance-backend/src/main/java/com/loloara/genreisromance/GenreisromanceApplication.java
+++ genreisromance-backend/src/main/java/com/loloara/genreisromance/GenreisromanceApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GenreisromanceApplication {
 
 	public static void main(String[] args) {
+		String profile = System.getProperty("spring.profiles.active");
+		if(profile == null){
+			System.setProperty("spring.profiles.active", "development");
+		}
+
 		SpringApplication.run(GenreisromanceApplication.class, args);
 	}
 
