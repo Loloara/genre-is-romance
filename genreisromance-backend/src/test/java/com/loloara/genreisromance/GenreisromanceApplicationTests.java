9c2f8c1c8d71480e8550ed8dc855d7deb3fcdb38
loloara <addy94@naver.com>
2020-02-06 00:16:10 +0900

    Active Profiles "test"

--- genreisromance-backend/src/test/java/com/loloara/genreisromance/GenreisromanceApplicationTests.java
+++ genreisromance-backend/src/test/java/com/loloara/genreisromance/GenreisromanceApplicationTests.java
@@ -2,8 +2,10 @@ package com.loloara.genreisromance;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GenreisromanceApplicationTests {
 
 	@Test
