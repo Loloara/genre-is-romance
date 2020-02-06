248fd42a1090b1ca8bbd52e6a7250574b039b362
loloara <addy94@naver.com>
2020-02-06 20:47:13 +0900

    test database connection & base properties

--- genreisromance-backend/src/test/java/com/loloara/genreisromance/DatabaseTest.java
+++ genreisromance-backend/src/test/java/com/loloara/genreisromance/DatabaseTest.java
@@ -1,12 +1,21 @@
 package com.loloara.genreisromance;
 
-import com.loloara.genreisromance.config.ApplicationProperties;
+import com.loloara.genreisromance.model.User;
+import com.loloara.genreisromance.repository.UserRepository;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+
+import java.sql.Connection;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -16,32 +25,37 @@ import static org.junit.Assert.assertEquals;
 public class DatabaseTest {
 
     @Autowired
-    ApplicationProperties.DataSource dataSource;
+    DataSource dataSource;
+
+    @Autowired
+    DataSourceProperties dataSourceProperties;
+
+    @Autowired
+    UserRepository userRepository;
 
     @Test
-    public void ApplicationPropertiesTest() {
-        boolean cachePrepStmts = true;
-        int prepStmtCacheSize = 250;
-        int prepStmtCacheSqlLimit = 2048;
-        boolean useServerPrepStmts = true;
-        boolean useLocalSessionState = true;
-        boolean rewriteBatchedStatements = true;
-        boolean cacheResultSetMetadata = true;
-        boolean cacheServerConfiguration = true;
-        boolean elideSetAutoCommits = true;
-        boolean maintainTimeStats = false;
+    public void dataSourcePropertiesTest() {
+        try(Connection con = dataSource.getConnection()) {
+            System.out.println("Database Connected: " + con);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String driverClassName = "com.mysql.cj.jdbc.Driver";
+        String jdbcUrl = "jdbc:mysql://localhost:3306/genreisromance?serverTimezone=Asia/Seoul&userUnicode=true&characterEncoding=utf-8";
+        String username = "root";
 
         System.out.println("DataSource.toString(): " + dataSource.toString());
-        assertEquals(cachePrepStmts, dataSource.isCachePrepStmts());
-        assertEquals(prepStmtCacheSize, dataSource.getPrepStmtCacheSize());
-        assertEquals(prepStmtCacheSqlLimit, dataSource.getPrepStmtCacheSqlLimit());
-        assertEquals(useLocalSessionState, dataSource.isUseLocalSessionState());
-        assertEquals(useServerPrepStmts, dataSource.isUseServerPrepStmts());
-        assertEquals(rewriteBatchedStatements, dataSource.isRewriteBatchedStatements());
-        assertEquals(cachePrepStmts, dataSource.isCachePrepStmts());
-        assertEquals(cacheResultSetMetadata, dataSource.isCacheResultSetMetadata());
-        assertEquals(cacheServerConfiguration, dataSource.isCacheServerConfiguration());
-        assertEquals(elideSetAutoCommits, dataSource.isElideSetAutoCommits());
-        assertEquals(maintainTimeStats, dataSource.isMaintainTimeStats());
+        assertEquals(driverClassName, dataSourceProperties.getDriverClassName());
+        assertEquals(jdbcUrl, dataSourceProperties.getUrl());
+        assertEquals(username, dataSourceProperties.getUsername());
+    }
+
+    @Test
+    public void userSaveTest() {
+        userRepository.save(new User(1L, "loloara", "Lucas"));
+        List<User> users = (List<User>) userRepository.findAll();
+
+        assertEquals(users.size(), 1);
     }
 }
