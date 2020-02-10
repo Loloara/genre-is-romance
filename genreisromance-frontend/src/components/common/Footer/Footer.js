8bb7a2f44089c40949777560f40ac888667c1403
loloara <addy94@naver.com>
2020-02-10 22:04:36 +0900

    exchange code line between classNames and styles

--- genreisromance-frontend/src/components/common/Footer/Footer.js
+++ genreisromance-frontend/src/components/common/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import styles from './Footer.scss';
 import classNames from 'classnames/bind';
+import styles from './Footer.scss';
 
 const cx = classNames.bind(styles);
 
