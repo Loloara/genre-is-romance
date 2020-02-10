828409813dad5b96f3ec2986bb08303da16cef04
loloara <addy94@naver.com>
2020-02-10 22:00:07 +0900

    change class component to functional component

--- genreisromance-frontend/src/components/common/Header/Header.js
+++ genreisromance-frontend/src/components/common/Header/Header.js
@@ -1,17 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classNames from 'classnames/bind';
 import styles from './Header.scss';
 
 const cx = classNames.bind(styles);
 
-class Header extends Component {
-    render() {
-        return(
-            <div>
-                I'm header
-            </div>
-        );
-    }
-}
+const Header = () => (
+    <header className={cx('header')}>
+        <h1 className={cx('content')}><a href="#;">영화같은 첫 만남, 장르는 로맨스</a></h1>
+    </header>
+);
 
 export default Header;
