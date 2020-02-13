fd558cf25543b67ea66d06e3d251679450623a4a
loloara <addy94@naver.com>
2020-02-13 21:50:48 +0900

    make User Entity

--- genreisromance-backend/src/main/java/com/loloara/genreisromance/model/User.java
+++ genreisromance-backend/src/main/java/com/loloara/genreisromance/model/User.java
@@ -1,12 +1,16 @@
 package com.loloara.genreisromance.model;
 
+import com.loloara.genreisromance.common.util.Gender;
+import com.loloara.genreisromance.common.util.ProcessType;
+import com.sun.istack.Nullable;
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import java.util.Date;
 
 import static lombok.AccessLevel.PROTECTED;
 
@@ -16,14 +20,45 @@ import static lombok.AccessLevel.PROTECTED;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = PROTECTED)
-public class User {
+public class User extends BaseModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Email
+    @Size(min = 5, max = 30)
     @NotNull
-    private String uid;
+    private String email;
 
     @NotNull
     private String userName;
+
+    @Nullable
+    private Integer height;
+
+    @NotNull
+    private String phone;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private Gender gender;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private ProcessType process;
+
+    @Transient
+    private Integer age;
+
+    @Temporal(TemporalType.DATE)
+    @NotNull
+    private Date birthDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @NotNull
+    private Date createdDate = new Date();
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @NotNull
+    private Date lastUpdatedDate = new Date();
 }
