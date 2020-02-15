1ae9862e7890349bc2fee77256aabb4567d40a9f
loloara <addy94@naver.com>
2020-02-15 16:48:53 +0900

    make UserService

---
+++ genreisromance-backend/src/main/java/com/loloara/genreisromance/service/UserService.java
@@ -0,0 +1,60 @@
+package com.loloara.genreisromance.service;
+
+import com.loloara.genreisromance.common.util.ProcessType;
+import com.loloara.genreisromance.model.User;
+import com.loloara.genreisromance.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public void saveAll(List<User> users) {
+        userRepository.saveAll(users);
+    }
+
+    public User findByUserId(Long userId) {
+        log.info("Find user by id : {}", userId);
+        return userRepository.findById(userId)
+                    .orElseThrow(IllegalArgumentException::new);
+    }
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    public User findByPhone(String phone) {
+        return userRepository.findByPhone(phone)
+                    .orElseThrow(IllegalArgumentException::new);
+    }
+
+    public List<User> findAllByUserId(List<Long> userIds) {
+        return userRepository.findAllById(userIds);
+    }
+
+    public List<Long> findAllByProcess(ProcessType process) {
+        log.info("Find userIds by process : {}", process);
+        return userRepository.findAllByProcess(process);
+    }
+
+    public boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
+
+    public boolean existsByPhone(String phone) {
+        return userRepository.existsByPhone(phone);
+    }
+}
