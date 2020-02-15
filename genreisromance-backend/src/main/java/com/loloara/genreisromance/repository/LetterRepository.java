c13e19d03b08153bd5896500777b5f9ee75b9114
loloara <addy94@naver.com>
2020-02-15 20:37:28 +0900

    add functions using user_id

--- genreisromance-backend/src/main/java/com/loloara/genreisromance/repository/LetterRepository.java
+++ genreisromance-backend/src/main/java/com/loloara/genreisromance/repository/LetterRepository.java
@@ -1,6 +1,7 @@
 package com.loloara.genreisromance.repository;
 
 import com.loloara.genreisromance.model.Letter;
+import com.loloara.genreisromance.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,6 @@ import java.util.Optional;
 
 @Repository
 public interface LetterRepository extends JpaRepository<Letter, Long> {
+    Optional<Letter> findByUser(User user);
+    boolean existsByUser(User user);
 }
