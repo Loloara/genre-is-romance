60e02d74950770425cfb8384ff425ecf111898c7
loloara <addy94@naver.com>
2020-02-17 21:36:16 +0900

    delete unused item from the library (com.loloara.genreisromance.model.User)

--- genreisromance-backend/src/main/java/com/loloara/genreisromance/service/LetterService.java
+++ genreisromance-backend/src/main/java/com/loloara/genreisromance/service/LetterService.java
@@ -1,7 +1,6 @@
 package com.loloara.genreisromance.service;
 
 import com.loloara.genreisromance.model.Letter;
-import com.loloara.genreisromance.model.User;
 import com.loloara.genreisromance.repository.LetterRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
