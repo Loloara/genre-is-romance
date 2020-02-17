09b524d6750a9d242c0549acc749d7738866247b
loloara <addy94@naver.com>
2020-02-17 21:29:22 +0900

    make queries for findByUserId, existsByUserId methods

--- genreisromance-backend/src/main/java/com/loloara/genreisromance/repository/LetterRepository.java
+++ genreisromance-backend/src/main/java/com/loloara/genreisromance/repository/LetterRepository.java
@@ -1,14 +1,18 @@
 package com.loloara.genreisromance.repository;
 
 import com.loloara.genreisromance.model.Letter;
-import com.loloara.genreisromance.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface LetterRepository extends JpaRepository<Letter, Long> {
-    Optional<Letter> findByUser(User user);
-    boolean existsByUser(User user);
+    @Query("select l from letter l where l.user = :userId;")
+    Optional<Letter> findByUserId(@Param("userId") Long userId);
+
+    @Query("select case when count(l) > 0 then true else false end from letter l where l.user = :userId;")
+    boolean existsByUserId(@Param("userId") Long userId);
 }
