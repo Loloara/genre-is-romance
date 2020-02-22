cff1dc63ad3717ca91428fc4bbbf6392a895de3b
loloara <addy94@naver.com>
2020-02-22 19:23:46 +0900

    make values in English

--- genreisromance-backend/src/main/java/com/loloara/genreisromance/common/util/ProcessType.java
+++ genreisromance-backend/src/main/java/com/loloara/genreisromance/common/util/ProcessType.java
@@ -1,11 +1,11 @@
 package com.loloara.genreisromance.common.util;
 
 public enum ProcessType {
-    찾는중,
-    제안도착,
-    프로포즈,
-    결제요청,
-    소개성사
+    SEARCHING,
+    SEARCHED,
+    PROPOSE,
+    PAYMENT,
+    SUCCESS
 }
 
     /* ToDo
