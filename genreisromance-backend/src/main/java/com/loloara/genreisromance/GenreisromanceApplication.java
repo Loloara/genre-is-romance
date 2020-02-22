4cb2bbc28bf83d3a7076c2dfed9aaeb3f391d37b
loloara <addy94@naver.com>
2020-02-22 19:23:11 +0900

    add log about current profile

--- genreisromance-backend/src/main/java/com/loloara/genreisromance/GenreisromanceApplication.java
+++ genreisromance-backend/src/main/java/com/loloara/genreisromance/GenreisromanceApplication.java
@@ -1,16 +1,21 @@
 package com.loloara.genreisromance;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class GenreisromanceApplication {
 
 	public static void main(String[] args) {
 		String profile = System.getProperty("spring.profiles.active");
-		if(profile == null){
+		if(profile == null) {
 			System.setProperty("spring.profiles.active", "development");
+			profile = "development";
 		}
+
+		log.info("Server Started with profile : {}", profile);
 
 		SpringApplication.run(GenreisromanceApplication.class, args);
 	}
