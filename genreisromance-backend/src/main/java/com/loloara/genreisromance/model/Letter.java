2141805155b6d7dfb46dc2673c6371875a7f1f6e
loloara <addy94@naver.com>
2020-02-22 19:15:31 +0900

    set parent class as BaseEntity & remove OnDelete annotation

--- genreisromance-backend/src/main/java/com/loloara/genreisromance/model/Letter.java
+++ genreisromance-backend/src/main/java/com/loloara/genreisromance/model/Letter.java
@@ -1,31 +1,21 @@
 package com.loloara.genreisromance.model;
 
+import com.loloara.genreisromance.common.util.ProcessType;
 import lombok.*;
-import org.hibernate.annotations.OnDelete;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import java.time.LocalDateTime;
-
 import static lombok.AccessLevel.PROTECTED;
-import static org.hibernate.annotations.OnDeleteAction.CASCADE;
 
 @Entity
-@Getter @Setter @ToString @Builder
+@Getter @ToString @Builder
 @AllArgsConstructor @NoArgsConstructor(access = PROTECTED)
-public class Letter extends BaseModel {
+public class Letter extends BaseEntity {
     /*
         mapping with id of User
      */
-
-    @Id
-    @NotNull
-    @Column(name = "letter_id")
-    private Long id;
 
     @Size(min = 0, max = 100)
     @Column(length = 100, nullable = false)
@@ -43,20 +33,18 @@ public class Letter extends BaseModel {
     @Column(name = "image_path", length = 18, nullable = false)
     private String imagePath;
 
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
     @NotNull
-    private Boolean pass = false;
+    private ProcessType process = ProcessType.SEARCHING;
 
-    @CreatedDate
-    @Column(name = "created_date", nullable = false)
-    private LocalDateTime createdDate = LocalDateTime.now();
-
-    @LastModifiedDate
-    @Column(name = "last_modified_date", nullable = false)
-    private LocalDateTime lastModifiedDate = LocalDateTime.now();
-
-    @OneToOne(fetch = FetchType.LAZY, optional = false)
-    @OnDelete(action = CASCADE)
+    @OneToOne(fetch = FetchType.LAZY, optional = false,
+            cascade = {
+                    CascadeType.MERGE,
+                    CascadeType.PERSIST,
+                    CascadeType.REMOVE
+            })
     @JoinColumn(name = "user_id")
     @NotNull
-    private User user;
+    private User user_id;
 }
