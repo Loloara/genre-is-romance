8f9f1df846580f466aaa42c5c3ac1c81e880e356
loloara <addy94@naver.com>
2020-02-22 20:35:17 +0900

    add UserController.java

---
+++ genreisromance-backend/src/main/java/com/loloara/genreisromance/api/UserController.java
@@ -0,0 +1,18 @@
+package com.loloara.genreisromance.api;
+
+import com.loloara.genreisromance.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+}
