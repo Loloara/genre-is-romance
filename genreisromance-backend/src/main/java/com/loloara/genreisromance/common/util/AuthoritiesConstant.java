06ab3e7b3f52e869d08f227ca7ab084a930790ad
loloara <addy94@naver.com>
2020-03-01 20:36:06 +0900

    add USER, ADMIN, ANONYMOUS in AuthoritiesConstant

---
+++ genreisromance-backend/src/main/java/com/loloara/genreisromance/common/util/AuthoritiesConstant.java
@@ -0,0 +1,8 @@
+package com.loloara.genreisromance.common.util;
+
+public class AuthoritiesConstant {
+
+    public static final String USER = "ROLE_USER";
+    public static final String ADMIN = "ROLE_ADMIN";
+    public static final String ANONYMOUS = "ROLE_ANONYMOUS";
+}
