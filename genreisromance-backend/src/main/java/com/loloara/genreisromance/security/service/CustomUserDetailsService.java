00c8bb6032708c28a425a5f121edbd4c906cf909
loloara <addy94@naver.com>
2020-03-01 20:37:38 +0900

    add CustomUserDetailsService implements UserDetailsService

---
+++ genreisromance-backend/src/main/java/com/loloara/genreisromance/security/service/CustomUserDetailsService.java
@@ -0,0 +1,43 @@
+package com.loloara.genreisromance.security.service;
+
+import com.loloara.genreisromance.common.exception.ApiException;
+import com.loloara.genreisromance.model.User;
+import com.loloara.genreisromance.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with email : " + email)
+                );
+
+        return CustomUserDetails.create(user);
+    }
+
+    @Transactional
+    public UserDetails loadUserById(Long id) {
+        User user = userRepository.findById(id).orElseThrow(
+                () -> new ApiException("User not found ", HttpStatus.NOT_FOUND)
+        );
+
+        return CustomUserDetails.create(user);
+    }
+}
