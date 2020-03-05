f4ce0149316bd0daf89364d2285bffa42475704f
loloara <addy94@naver.com>
2020-03-05 23:41:54 +0900

    add registerAccount service logic

--- genreisromance-backend/src/main/java/com/loloara/genreisromance/service/UserService.java
+++ genreisromance-backend/src/main/java/com/loloara/genreisromance/service/UserService.java
@@ -1,10 +1,21 @@
 package com.loloara.genreisromance.service;
 
+import com.loloara.genreisromance.common.exception.ApiException;
+import com.loloara.genreisromance.common.util.AuthProvider;
+import com.loloara.genreisromance.common.util.AuthoritiesConstant;
+import com.loloara.genreisromance.common.util.Gender;
 import com.loloara.genreisromance.common.util.ProcessType;
-import com.loloara.genreisromance.model.User;
+import com.loloara.genreisromance.model.domain.User;
+import com.loloara.genreisromance.model.domain.UserAuthority;
+import com.loloara.genreisromance.model.dto.UserDto;
+import com.loloara.genreisromance.repository.AuthorityRepository;
+import com.loloara.genreisromance.repository.UserAuthorityRepository;
 import com.loloara.genreisromance.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,9 +23,40 @@ import java.util.List;
 @Slf4j
 public class UserService {
     private final UserRepository userRepository;
+    private final AuthorityRepository authorityRepository;
+    private final UserAuthorityRepository userAuthorityRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository){
+    public UserService(UserRepository userRepository, AuthorityRepository authorityRepository, UserAuthorityRepository userAuthorityRepository, PasswordEncoder passwordEncoder){
         this.userRepository = userRepository;
+        this.authorityRepository = authorityRepository;
+        this.userAuthorityRepository = userAuthorityRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    public User registerAccount(UserDto.Create userDto) {
+        if(userRepository.existsByEmail(userDto.getEmail())) {
+            throw new ApiException("Email Already exists.", HttpStatus.BAD_REQUEST);
+        }
+        User user = userRepository.save(User.builder()
+                .email(userDto.getEmail())
+                .password(passwordEncoder.encode(userDto.getPassword()))
+                .userName(userDto.getUserName())
+                .height(userDto.getHeight())
+                .phone(userDto.getPhone())
+                .gender(Gender.fromValue(userDto.getGender()))
+                .birthDate(userDto.getBirthDate())
+                .provider(AuthProvider.LOCAL)
+                .build());
+
+        authorityRepository.findById(AuthoritiesConstant.USER).ifPresent(
+                auth -> {
+                    UserAuthority userAuthority = userAuthorityRepository.save(
+                            UserAuthority.builder().user(user).authority(auth).build());
+        });
+
+        return user;
     }
 
     public User save(User user) {
