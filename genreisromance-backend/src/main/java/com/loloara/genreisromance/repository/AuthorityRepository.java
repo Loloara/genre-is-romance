27c6495cbe61534bb340bfe2721675efa037db41
loloara <addy94@naver.com>
2020-03-05 23:39:32 +0900

    add findByNameFetchAll query

--- genreisromance-backend/src/main/java/com/loloara/genreisromance/repository/AuthorityRepository.java
+++ genreisromance-backend/src/main/java/com/loloara/genreisromance/repository/AuthorityRepository.java
@@ -1,9 +1,16 @@
 package com.loloara.genreisromance.repository;
 
-import com.loloara.genreisromance.model.Authority;
+import com.loloara.genreisromance.model.domain.Authority;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface AuthorityRepository extends JpaRepository<Authority, String> {
+
+    @Query("select a from Authority a join fetch a.users where a.name = :name")
+    Optional<Authority> findByNameFetchAll(@Param("name") String name);
 }
