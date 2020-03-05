37d80953bf22c5f89d7e3589c187781c4bd5b11e
loloara <addy94@naver.com>
2020-03-05 23:39:02 +0900

    add deleteByEmail query

--- genreisromance-backend/src/main/java/com/loloara/genreisromance/repository/UserRepository.java
+++ genreisromance-backend/src/main/java/com/loloara/genreisromance/repository/UserRepository.java
@@ -1,12 +1,14 @@
 package com.loloara.genreisromance.repository;
 
 import com.loloara.genreisromance.common.util.ProcessType;
-import com.loloara.genreisromance.model.User;
+import com.loloara.genreisromance.model.domain.User;
 import com.sun.tools.javac.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -20,9 +22,16 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Optional<User> findByPhone(String phone);
 
+    @Query("select u from User u join fetch u.authorities where u.email = :email")
+    Optional<User> findByEmailFetchAll(@Param("email") String email);
+
     @Query("select id from User where process = :process")
     List<Long> findAllByProcess(@Param("process") ProcessType process);
 
     boolean existsByEmail(String email);
     boolean existsByPhone(String phone);
+
+    @Transactional
+    @Modifying
+    void deleteByEmail(String email);
 }
