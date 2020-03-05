70ef7ba36c991e274b4dbe3c56c39f3950d22f72
loloara <addy94@naver.com>
2020-03-05 23:39:53 +0900

    add UserAuthority repository

---
+++ genreisromance-backend/src/main/java/com/loloara/genreisromance/repository/UserAuthorityRepository.java
@@ -0,0 +1,8 @@
+package com.loloara.genreisromance.repository;
+
+import com.loloara.genreisromance.model.domain.UserAuthority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAuthorityRepository extends JpaRepository<UserAuthority, Long> {
+
+}
