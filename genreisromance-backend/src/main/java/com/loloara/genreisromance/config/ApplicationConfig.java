40407603612c3f94f6d9284816b1170474c993af
loloara <addy94@naver.com>
2020-03-05 23:36:19 +0900

    add ApplicationConfig Configuration to add Bean ModelMapper

---
+++ genreisromance-backend/src/main/java/com/loloara/genreisromance/config/ApplicationConfig.java
@@ -0,0 +1,14 @@
+package com.loloara.genreisromance.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
