cd3a7cf0f01f09eb57c96c9ea5235118f3a51536
loloara <addy94@naver.com>
2020-03-05 23:26:26 +0900

    add functions for json

--- genreisromance-backend/src/main/java/com/loloara/genreisromance/common/util/Gender.java
+++ genreisromance-backend/src/main/java/com/loloara/genreisromance/common/util/Gender.java
@@ -1,6 +1,33 @@
 package com.loloara.genreisromance.common.util;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.util.Arrays;
+
 public enum Gender {
-    MALE,
-    FEMALE
+
+    MALE("MALE"), FEMALE("FEMALE");
+
+    private String value;
+
+    private Gender(String value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    public String jsonValue() {
+        return value;
+    }
+
+    @JsonCreator
+    public static Gender fromValue(String value) {
+        for(Gender gender : values()) {
+            if(gender.value.equalsIgnoreCase(value)) {
+                return gender;
+            }
+        }
+        throw new IllegalArgumentException(
+                "Unknown enum type " + value + ", Allowed values are "  + Arrays.toString(values()));
+    }
 }
