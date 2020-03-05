62748cadbabb1d7250b248e9b8d3e7c603a01560
loloara <addy94@naver.com>
2020-03-05 23:31:53 +0900

    add Set<UserAuthority> users for OneToMany relation

---
+++ genreisromance-backend/src/main/java/com/loloara/genreisromance/model/domain/Authority.java
@@ -0,0 +1,55 @@
+package com.loloara.genreisromance.model.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.loloara.genreisromance.model.BaseModel;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity @Getter @Builder
+@AllArgsConstructor @NoArgsConstructor(access = PROTECTED)
+public class Authority extends BaseModel {
+
+    @Id
+    @Size(min = 0, max = 20)
+    @Column(length = 20, nullable = false)
+    private String name;
+
+    @Builder.Default
+    @JsonIgnore
+    @OneToMany(mappedBy = "authority", fetch = FetchType.LAZY,
+            cascade = {
+                    CascadeType.MERGE
+            })
+    private Set<UserAuthority> users = new HashSet<>();
+
+    @PreRemove
+    public void preRemove() {
+        for(UserAuthority userAuthority : users) {
+            userAuthority.setNullAuthority();
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) {
+            return true;
+        }
+        if(o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Authority authority = (Authority) o;
+        return name.equals(authority.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
+}
