e926a24e40de208622d97e390e0187783fa3b913
loloara <addy94@naver.com>
2020-03-06 22:21:47 +0900

    remove unused service logics

--- genreisromance-backend/src/main/java/com/loloara/genreisromance/service/UserService.java
+++ genreisromance-backend/src/main/java/com/loloara/genreisromance/service/UserService.java
@@ -4,7 +4,6 @@ import com.loloara.genreisromance.common.exception.ApiException;
 import com.loloara.genreisromance.common.util.AuthProvider;
 import com.loloara.genreisromance.common.util.AuthoritiesConstant;
 import com.loloara.genreisromance.common.util.Gender;
-import com.loloara.genreisromance.common.util.ProcessType;
 import com.loloara.genreisromance.model.domain.User;
 import com.loloara.genreisromance.model.domain.UserAuthority;
 import com.loloara.genreisromance.model.dto.UserDto;
@@ -16,8 +15,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @Slf4j
@@ -57,59 +54,5 @@ public class UserService {
         });
 
         return user;
-    }
-
-    public User save(User user) {
-        return userRepository.save(user);
-    }
-
-    public void saveAll(List<User> users) {
-        userRepository.saveAll(users);
-    }
-
-    public User findByUserId(Long userId) {
-        log.info("Find user by id : {}", userId);
-        return userRepository.findById(userId)
-                    .orElseThrow(IllegalArgumentException::new);
-    }
-
-    public User findByEmail(String email) {
-        return userRepository.findByEmail(email)
-                .orElseThrow(IllegalArgumentException::new);
-    }
-
-    public User findByPhone(String phone) {
-        return userRepository.findByPhone(phone)
-                    .orElseThrow(IllegalArgumentException::new);
-    }
-
-    public List<User> findAllByUserId(List<Long> userIds) {
-        return userRepository.findAllById(userIds);
-    }
-
-    public List<Long> findAllByProcess(ProcessType process) {
-        log.info("Find userIds by process : {}", process);
-        return userRepository.findAllByProcess(process);
-    }
-
-    public List<User> findAll() {
-        return userRepository.findAll();
-    }
-
-    public boolean existsById (Long userId) {
-        return userRepository.existsById(userId);
-    }
-
-    public boolean existsByEmail(String email) {
-        return userRepository.existsByEmail(email);
-    }
-
-    public boolean existsByPhone(String phone) {
-        return userRepository.existsByPhone(phone);
-    }
-
-    public void delete(User user) {
-        log.info("Delete user by object User : {}", user.getId());
-        userRepository.delete(user);
     }
 }
