b74946609e9a7b895b248658859738f94b548106
loloara <addy94@naver.com>
2020-03-09 20:03:17 +0900

    add LoginRequest dto for AuthController

--- genreisromance-backend/src/main/java/com/loloara/genreisromance/model/dto/UserDto.java
+++ genreisromance-backend/src/main/java/com/loloara/genreisromance/model/dto/UserDto.java
@@ -55,4 +55,17 @@ public class UserDto {
             this.authorities = authorities.stream().map( userAuthority -> userAuthority.getAuthority().getName()).collect(Collectors.toSet());
         }
     }
+
+    @Getter @Builder
+    public static class LoginRequest {
+
+        @NotBlank(message = "이메일은 필수 입력 값입니다.")
+        @Email(message = "이메일 형식에 맞지 않습니다.")
+        private String email;
+
+        @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+        @Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
+                message = "비밀번호는 영어와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 20자의 비밀번호여야 합니다.")
+        private String password;
+    }
 }
