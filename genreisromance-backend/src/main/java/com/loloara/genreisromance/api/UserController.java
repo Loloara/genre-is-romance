50164c843c68fe9f35d3dd05639c282968796075
loloara <addy94@naver.com>
2020-03-11 23:28:29 +0900

    remove @CrossOrigin

--- genreisromance-backend/src/main/java/com/loloara/genreisromance/api/UserController.java
+++ genreisromance-backend/src/main/java/com/loloara/genreisromance/api/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @Slf4j
-@RestController @CrossOrigin
+@RestController
 @RequestMapping(path = "/api")
 public class UserController {
 
@@ -51,4 +51,6 @@ public class UserController {
                 .map(response -> ResponseEntity.ok().body(response))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
+
+
 }
