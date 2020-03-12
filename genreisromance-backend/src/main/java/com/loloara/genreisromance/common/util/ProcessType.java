18de426ba2341a7655163ba28cb7242e40534b50
loloara <addy94@naver.com>
2020-03-12 22:34:13 +0900

    add fromInteger method for api call

--- genreisromance-backend/src/main/java/com/loloara/genreisromance/common/util/ProcessType.java
+++ genreisromance-backend/src/main/java/com/loloara/genreisromance/common/util/ProcessType.java
@@ -1,11 +1,27 @@
 package com.loloara.genreisromance.common.util;
 
+import java.util.Arrays;
+
 public enum ProcessType {
-    SEARCHING,
-    SEARCHED,
-    PROPOSE,
-    PAYMENT,
-    SUCCESS
+
+    SEARCHING("SEARCHING"), SEARCHED("SEARCHED"), PROPOSE("PROPOSE"), PAYMENT("PAYMENT"), SUCCESS("SUCCESS");
+
+    private String value;
+
+    private ProcessType(String value) {
+        this.value = value;
+    }
+
+    public static ProcessType fromInteger(int value) {
+        for(ProcessType process : values()) {
+            if(process.ordinal() == value) {
+                return process;
+            }
+        }
+
+        throw new IllegalArgumentException(
+                "Unknown enum type " + value + ", Allowed values are "  + Arrays.toString(values()));
+    }
 }
 
     /* ToDo
