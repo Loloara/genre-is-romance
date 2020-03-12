c09edc4308bf7cae03ce0390223848f0e660a384
loloara <addy94@naver.com>
2020-03-12 23:52:57 +0900

    add getLetter with current user, update letter apis

--- genreisromance-backend/src/main/java/com/loloara/genreisromance/api/LetterController.java
+++ genreisromance-backend/src/main/java/com/loloara/genreisromance/api/LetterController.java
@@ -1,8 +1,11 @@
 package com.loloara.genreisromance.api;
 
 import com.loloara.genreisromance.common.annotation.CurrentUser;
+import com.loloara.genreisromance.common.exception.ApiException;
 import com.loloara.genreisromance.common.util.ProcessType;
+import com.loloara.genreisromance.model.domain.Letter;
 import com.loloara.genreisromance.model.dto.LetterDto;
+import com.loloara.genreisromance.repository.LetterRepository;
 import com.loloara.genreisromance.security.service.CustomUserDetails;
 import com.loloara.genreisromance.service.LetterService;
 import io.swagger.annotations.ApiOperation;
@@ -17,9 +20,11 @@ import org.springframework.web.bind.annotation.*;
 public class LetterController {
 
     private final LetterService letterService;
+    private final LetterRepository letterRepository;
 
-    public LetterController(LetterService letterService) {
+    public LetterController(LetterService letterService, LetterRepository letterRepository) {
         this.letterService = letterService;
+        this.letterRepository= letterRepository;
     }
 
     @ApiOperation(value = "create letter", notes = "create letter with current user")
@@ -38,5 +43,33 @@ public class LetterController {
         log.debug("REST request to get Letters by process : {}", processType);
         LetterDto.LetterInfos response = letterService.findByProcess(processType);
         return new ResponseEntity<LetterDto.LetterInfos>(response, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "get letter of current user", notes = "현재 유저의 letter 얻기")
+    @GetMapping("/{userId}")
+    public ResponseEntity<LetterDto.LetterInfo> getLetter(@PathVariable Long userId) {
+        log.debug("REST request to get Letter of current user : {}", userId);
+        Letter letter = letterRepository.findByUserId(userId)
+                .orElseThrow(() -> new ApiException("Letter does not exist", HttpStatus.NOT_FOUND));
+        LetterDto.LetterInfo response = new LetterDto.LetterInfo(letter);
+        return new ResponseEntity<LetterDto.LetterInfo>(response, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "update letter", notes = "letter 수정")
+    @PutMapping
+    public ResponseEntity<LetterDto.LetterInfo> getLettersByProcess(@RequestBody LetterDto.Update letterDto,
+                                                                    @CurrentUser CustomUserDetails customUserDetails) {
+        log.debug("REST request to update Letter of current user : {}", customUserDetails.getUsername());
+        LetterDto.LetterInfo response = letterService.updateLetter(letterDto, customUserDetails);
+        return new ResponseEntity<LetterDto.LetterInfo>(response, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "update process of letter by userId", notes = "관리자가 letter process 수정")
+    @PutMapping("/admin/{userId}")
+    public ResponseEntity<LetterDto.LetterInfo> getLettersByProcess(@PathVariable Long userId,
+                                                                    @RequestBody LetterDto.UpdateProcess letterDto) {
+        log.debug("REST request to update Letter of current user : {}", userId);
+        LetterDto.LetterInfo response = letterService.updateLetterProcess(letterDto, userId);
+        return new ResponseEntity<LetterDto.LetterInfo>(response, HttpStatus.OK);
     }
 }
