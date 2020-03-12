d6d068558d8d58c0510134f4adee98d0d0410e46
loloara <addy94@naver.com>
2020-03-12 22:44:48 +0900

    add findByProcess method

--- genreisromance-backend/src/main/java/com/loloara/genreisromance/repository/LetterRepository.java
+++ genreisromance-backend/src/main/java/com/loloara/genreisromance/repository/LetterRepository.java
@@ -1,11 +1,13 @@
 package com.loloara.genreisromance.repository;
 
+import com.loloara.genreisromance.common.util.ProcessType;
 import com.loloara.genreisromance.model.domain.Letter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,6 +15,8 @@ public interface LetterRepository extends JpaRepository<Letter, Long> {
 
     @Query("select l from Letter l where l.user_id = :userId")
     Optional<Letter> findByUserId(@Param("userId") Long userId);
+
+    List<Letter> findByProcess(ProcessType process);
 
     @Query("select case when count(l) > 0 then true else false end from Letter l where l.user_id = :userId")
     boolean existsByUserId(@Param("userId") Long userId);
