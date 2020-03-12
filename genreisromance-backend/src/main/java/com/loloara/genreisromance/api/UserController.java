b9113ba6b98512a843991826a95a49d0ac35f4f1
loloara <addy94@naver.com>
2020-03-12 22:46:33 +0900

    add isAdmin parameter to registerAccount & change api urls

--- genreisromance-backend/src/main/java/com/loloara/genreisromance/api/UserController.java
+++ genreisromance-backend/src/main/java/com/loloara/genreisromance/api/UserController.java
@@ -1,5 +1,6 @@
 package com.loloara.genreisromance.api;
 
+import com.loloara.genreisromance.common.util.AuthoritiesConstant;
 import com.loloara.genreisromance.model.domain.User;
 import com.loloara.genreisromance.model.dto.UserDto;
 import com.loloara.genreisromance.repository.UserRepository;
@@ -16,7 +17,7 @@ import javax.validation.Valid;
 
 @Slf4j
 @RestController
-@RequestMapping(path = "/api")
+@RequestMapping(path = "/api/user")
 public class UserController {
 
     private final UserService userService;
@@ -32,22 +33,24 @@ public class UserController {
     private static final String CHECK_ERROR_MESSAGE = "Incorrect password";
 
     @ApiOperation(value = "Email 회원가입 API", notes = "Authorization Header 필요 없습니다.")
-    @PostMapping(path = "/register")
-    public ResponseEntity<?> registerAccount(@Valid @RequestBody UserDto.Create userDto) throws IllegalArgumentException {
+    @PostMapping
+    public ResponseEntity<?> registerAccount(@Valid @RequestBody UserDto.Create userDto,
+                                             @RequestParam(value = "isAdmin", defaultValue = "false", required = false) boolean isAdmin) throws IllegalArgumentException {
         if(StringUtils.isEmpty(userDto.getPassword()) &&
                 (userDto.getPassword().length() < 8 || userDto.getPassword().length() > 20)) {
             return new ResponseEntity<>(CHECK_ERROR_MESSAGE, HttpStatus.BAD_REQUEST);
         }
-        User result = userService.registerAccount(userDto);
+        String role = isAdmin ? AuthoritiesConstant.ADMIN : AuthoritiesConstant.USER;
+        User result = userService.registerAccount(userDto, role);
 
         return new ResponseEntity<User>(result, HttpStatus.CREATED);
     }
 
     @ApiOperation(value = "get User by Email API", notes = "Authorization Header 필요 없습니다.")
-    @GetMapping(path = "/user/{email}")
-    public ResponseEntity<UserDto.Response> getUser(@PathVariable String email) {
+    @GetMapping(path = "/{email}")
+    public ResponseEntity<UserDto.UserInfo> getUser(@PathVariable String email) {
         return userRepository.findByEmail(email)
-                .map(user -> modelMapper.map(user, UserDto.Response.class))
+                .map(user -> modelMapper.map(user, UserDto.UserInfo.class))
                 .map(response -> ResponseEntity.ok().body(response))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
