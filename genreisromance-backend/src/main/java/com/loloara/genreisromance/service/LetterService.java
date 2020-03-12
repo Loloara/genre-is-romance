55ed85778905331b7a2af00694422f9fe68de076
loloara <addy94@naver.com>
2020-03-12 23:52:11 +0900

    add updateLetter and updateLetterProcess logic

--- genreisromance-backend/src/main/java/com/loloara/genreisromance/service/LetterService.java
+++ genreisromance-backend/src/main/java/com/loloara/genreisromance/service/LetterService.java
@@ -1,5 +1,6 @@
 package com.loloara.genreisromance.service;
 
+import com.loloara.genreisromance.common.exception.ApiException;
 import com.loloara.genreisromance.common.util.ProcessType;
 import com.loloara.genreisromance.model.domain.Letter;
 import com.loloara.genreisromance.model.domain.User;
@@ -7,7 +8,9 @@ import com.loloara.genreisromance.model.dto.LetterDto;
 import com.loloara.genreisromance.repository.LetterRepository;
 import com.loloara.genreisromance.security.service.CustomUserDetails;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
@@ -33,5 +36,23 @@ public class LetterService {
 
     public LetterDto.LetterInfos findByProcess(ProcessType processType) {
         return new LetterDto.LetterInfos(letterRepository.findByProcess(processType));
+    }
+
+    @Transactional
+    public LetterDto.LetterInfo updateLetter(LetterDto.Update letterDto, CustomUserDetails customUserDetails) {
+        Letter letter = letterRepository.findByUserId(customUserDetails.getId())
+                .orElseThrow(() -> new ApiException("Letter does not exist", HttpStatus.NOT_FOUND));
+        letter.updateVal(letterDto);
+
+        return new LetterDto.LetterInfo(letter);
+    }
+
+    @Transactional
+    public LetterDto.LetterInfo updateLetterProcess(LetterDto.UpdateProcess letterDto, Long userId) {
+        Letter letter = letterRepository.findByUserId(userId)
+                .orElseThrow(() -> new ApiException("Letter does not exist", HttpStatus.NOT_FOUND));
+        letter.setProcess(letterDto.getProcess());
+
+        return new LetterDto.LetterInfo(letter);
     }
 }
