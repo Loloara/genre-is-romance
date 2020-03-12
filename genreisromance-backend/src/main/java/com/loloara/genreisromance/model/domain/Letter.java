60d7bf92bcc41fe1f39da3a10f1636bf7c91b6b7
loloara <addy94@naver.com>
2020-03-12 23:51:14 +0900

    add setProcess and updateVal

--- genreisromance-backend/src/main/java/com/loloara/genreisromance/model/domain/Letter.java
+++ genreisromance-backend/src/main/java/com/loloara/genreisromance/model/domain/Letter.java
@@ -2,6 +2,7 @@ package com.loloara.genreisromance.model.domain;
 
 import com.loloara.genreisromance.common.util.ProcessType;
 import com.loloara.genreisromance.model.BaseEntity;
+import com.loloara.genreisromance.model.dto.LetterDto;
 import lombok.*;
 
 import javax.persistence.*;
@@ -42,4 +43,31 @@ public class Letter extends BaseEntity {
     @JoinColumn(name = "user_id")
     @NotNull
     private User user_id;
+
+    public void setProcess(ProcessType process) {
+        this.process = process;
+    }
+
+    public boolean updateVal(LetterDto.Update letterDto) {
+        String newImagePath = letterDto.getImagePath();
+        String newQ1 = letterDto.getQ1();
+        String newQ2 = letterDto.getQ2();
+        String newQ3 = letterDto.getQ3();
+        if(newImagePath == null && newQ1 == null && newQ2 == null && newQ3 == null) {
+            return false;
+        }
+        if(newImagePath != null) {
+            this.imagePath = newImagePath;
+        }
+        if(newQ1 != null) {
+            this.q1 = newQ1;
+        }
+        if(newQ2 != null) {
+            this.q2 = newQ2;
+        }
+        if(newQ3 != null) {
+            this.q3 = newQ3;
+        }
+        return true;
+    }
 }
