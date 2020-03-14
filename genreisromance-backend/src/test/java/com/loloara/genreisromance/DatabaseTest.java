b8914129dcd28b901c727e3f8f13d880434c2102
loloara <addy94@naver.com>
2020-03-14 13:23:37 +0900

    using *Repository instead of *Service for TEST

--- genreisromance-backend/src/test/java/com/loloara/genreisromance/DatabaseTest.java
+++ genreisromance-backend/src/test/java/com/loloara/genreisromance/DatabaseTest.java
@@ -1,16 +1,21 @@
 package com.loloara.genreisromance;
 
+import com.loloara.genreisromance.common.exception.ApiException;
+import com.loloara.genreisromance.common.util.AuthProvider;
+import com.loloara.genreisromance.common.util.AuthoritiesConstant;
 import com.loloara.genreisromance.common.util.DayTime;
 import com.loloara.genreisromance.common.util.Gender;
-import com.loloara.genreisromance.model.*;
-import com.loloara.genreisromance.service.*;
+import com.loloara.genreisromance.model.domain.*;
+import com.loloara.genreisromance.repository.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.sql.DataSource;
 
@@ -18,7 +23,6 @@ import java.sql.Connection;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 import static org.junit.Assert.*;
 
@@ -34,31 +38,40 @@ public class DatabaseTest {
     DataSourceProperties dataSourceProperties;
 
     @Autowired
-    UserService userService;
+    UserRepository userRepository;
 
     @Autowired
-    LetterService letterService;
+    PasswordEncoder passwordEncoder;
 
     @Autowired
-    MatchInfoService matchInfoService;
+    AuthorityRepository authorityRepository;
 
     @Autowired
-    MatchMovieService matchMovieService;
+    UserAuthorityRepository userAuthorityRepository;
 
     @Autowired
-    MatchPlaceService matchPlaceService;
+    LetterRepository letterRepository;
 
     @Autowired
-    MatchTheDayService matchTheDayService;
+    MatchInfoRepository matchInfoRepository;
 
     @Autowired
-    MovieService movieService;
+    MatchMovieRepository matchMovieRepository;
 
     @Autowired
-    PlaceService placeService;
+    MatchPlaceRepository matchPlaceRepository;
 
     @Autowired
-    TheDayService theDayService;
+    MatchTheDayRepository matchTheDayRepository;
+
+    @Autowired
+    MovieRepository movieRepository;
+
+    @Autowired
+    PlaceRepository placeRepository;
+
+    @Autowired
+    TheDayRepository theDayRepository;
 
     @Test
     public void dataSourcePropertiesTest() {
@@ -80,86 +93,104 @@ public class DatabaseTest {
 
     @Test
     public void userLetterSaveTest() {
-        User user = userService.save(
+        User user = userRepository.save(
                 User.builder()
-                        .userName("Lucas")
+                        .userName("test99")
                         .birthDate(LocalDate.of(1994,4,10))
-                        .email("test@gmail.com")
+                        .email("test99@gmail.com")
                         .gender(Gender.MALE)
                         .height(173)
-                        .phone("01012345678")
+                        .phone("01043215678")
+                        .provider(AuthProvider.LOCAL)
+                        .password(passwordEncoder.encode("test1@3$"))
                         .build()
         );
 
-        Letter letter = letterService.save(
+        User finalUser = user;
+        Authority auth = authorityRepository.findById(AuthoritiesConstant.USER).orElseThrow(() -> new ApiException("no auth"));
+        UserAuthority uAuth = userAuthorityRepository.save(UserAuthority.builder().user(finalUser).authority(auth).build());
+
+        Letter letter = letterRepository.save(
                 Letter.builder()
                         .user_id(user)
-                        .Q1("Q1 TEST")
-                        .Q2("Q2 TEST")
-                        .Q3("Q3 TEST")
+                        .q1("Q1 TEST")
+                        .q2("Q2 TEST")
+                        .q3("Q3 TEST")
                         .imagePath("img_path_min_15")
                         .build()
         );
 
         System.out.println("===================USER SAVE TEST===================");
-        user = userService.findByUserId(user.getId());
+        user = userRepository.findById(user.getId()).orElseThrow(() -> new ApiException("user not found"));
         System.out.println("user : " + user.toString());
 
         System.out.println("===================LETTER SAVE TEST===================");
-        letter = letterService.findById(letter.getId());
+        letter = letterRepository.findById(letter.getId()).orElseThrow(() -> new ApiException("letter not found"));
         System.out.println("letter : " + letter.toString());
 
-        boolean isSameUserId = letter.getUser_id().equals(user);
-        assertTrue(isSameUserId);
+        Long userId = user.getId();
+        Long letterId = letter.getId();
 
-        letterService.delete(letter);
+        assertEquals(letter.getUser_id(), user);
 
-        assertFalse(userService.existsById(user.getId()));
-        assertFalse(letterService.existsById(letter.getId()));
+        deleteForUserLetterSaveTest(letterId);
+
+        assertFalse(letterRepository.existsById(userId));
+        assertFalse(letterRepository.existsById(letterId));
+    }
+
+    @Transactional
+    public void deleteForUserLetterSaveTest(Long letterId) {
+        letterRepository.deleteById(letterId);
     }
 
     @Test
     public void userMatchTest() {
+        final String TEST_EMAIL_MALE = "test11@gmail.com";
+        final String TEST_EMAIL_FEMALE = "test12@gmail.com";
         List<User> users = new ArrayList<>();
         users.add(User.builder()
                 .userName("userM")
                 .birthDate(LocalDate.of(1994,4,10))
-                .email("test01@gmail.com")
+                .email(TEST_EMAIL_MALE)
                 .gender(Gender.MALE)
                 .height(173)
-                .phone("01012345678")
+                .phone("01023232323")
+                .provider(AuthProvider.LOCAL)
+                .password(passwordEncoder.encode("qwer1@3$"))
                 .build());
         users.add(User.builder()
                 .userName("userF")
-                .phone("01043218765")
+                .phone("01032323232")
                 .height(166)
                 .gender(Gender.FEMALE)
-                .email("test02@gmail.com")
+                .email(TEST_EMAIL_FEMALE)
                 .birthDate(LocalDate.of(1994,3,16))
+                .provider(AuthProvider.LOCAL)
+                .password(passwordEncoder.encode("qwer1@3$"))
                 .build());
-        userService.saveAll(users);
-
+        userRepository.saveAll(users);
 
         List<Letter> letters = new ArrayList<>();
         letters.add(Letter.builder()
                 .user_id(users.get(users.size()-2))
                 .imagePath("img_path_min_15")
-                .Q1("Q1 for Male")
-                .Q2("Q2 for Male")
-                .Q3("Q3 for Male")
+                .q1("Q1 for Male")
+                .q2("Q2 for Male")
+                .q3("Q3 for Male")
                 .build());
         letters.add(Letter.builder()
                 .user_id(users.get(users.size()-1))
                 .imagePath("img_path_min_15")
-                .Q1("Q1 for Female")
-                .Q2("Q2 for Female")
-                .Q3("Q3 for Female")
+                .q1("Q1 for Female")
+                .q2("Q2 for Female")
+                .q3("Q3 for Female")
                 .build());
-        letterService.saveAll(letters);
+        letterRepository.saveAll(letters);
 
-        User userM = userService.findByEmail("test01@gmail.com");
-        User userW = userService.findByEmail("test02@gmail.com");
-        MatchInfo matchInfo = matchInfoService.save(
+        User userM = userRepository.findByEmail(TEST_EMAIL_MALE).orElseThrow(() -> new ApiException("Not Found"));
+        User userW = userRepository.findByEmail(TEST_EMAIL_FEMALE).orElseThrow(() -> new ApiException("Not Found"));
+        MatchInfo matchInfo = matchInfoRepository.save(
                 MatchInfo.builder()
                         .userMaleId(userM)
                         .userFemaleId(userW)
@@ -167,35 +198,35 @@ public class DatabaseTest {
         );
 
         for(int i=1;i<=3;i++){
-            Movie movie = movieService.save(
+            Movie movie = movieRepository.save(
                     Movie.builder()
                             .movieTitle("movie_0" + i)
                             .build());
-            MatchMovie matchMovie = matchMovieService.save(
+            MatchMovie matchMovie = matchMovieRepository.save(
                     MatchMovie.builder()
                             .movie(movie)
                             .matchInfo(matchInfo)
                             .build()
             );
 
-            Place place = placeService.save(
+            Place place = placeRepository.save(
                     Place.builder()
                             .placeName("place_0" + i)
                             .build());
-            MatchPlace matchPlace = matchPlaceService.save(
+            MatchPlace matchPlace = matchPlaceRepository.save(
                     MatchPlace.builder()
                             .place(place)
                             .matchInfo(matchInfo)
                             .build()
             );
 
-            TheDay theDay = theDayService.save(
+            TheDay theDay = theDayRepository.save(
                     TheDay.builder()
                             .dayDate(LocalDate.of(2020, 3, 16+i-1))
                             .dayTime(DayTime._7PM)
                             .build()
             );
-            MatchTheDay matchTheDay = matchTheDayService.save(
+            MatchTheDay matchTheDay = matchTheDayRepository.save(
                     MatchTheDay.builder()
                             .theDay(theDay)
                             .matchInfo(matchInfo)
@@ -203,14 +234,14 @@ public class DatabaseTest {
             );
         }
 
-        matchInfo = matchInfoService.findByIdFecthAll(matchInfo.getId());
+        matchInfo = matchInfoRepository.findByIdFetchAll(matchInfo.getId()).orElseThrow(() -> new ApiException("Not Found"));
 
         assertEquals(matchInfo.getUserMaleId().getUserName(), userM.getUserName());
         assertEquals(matchInfo.getUserFemaleId().getUserName(), userW.getUserName());
 
         for(MatchTheDay mt : matchInfo.getThe_days()){
             System.out.println("logging_theDays_of_matchInfo : "+mt.getId()+" : " + mt.getTheDay().getId()+"_"+mt.getTheDay().getDayDate());
-            TheDay theDay = theDayService.findByIdFetchAll(mt.getTheDay().getId());
+            TheDay theDay = theDayRepository.findByIdFetchAll(mt.getTheDay().getId()).orElseThrow(() -> new ApiException("Not Found"));
             assertEquals(mt.getTheDay().getId(), theDay.getId());
             for(MatchTheDay mt2 : theDay.getMatchTheDays()) {
                 System.out.println("logging_theDay : " + theDay.getId()+ " : " + mt2.getId());
@@ -220,7 +251,7 @@ public class DatabaseTest {
 
         for(MatchPlace mp : matchInfo.getPlaces()){
             System.out.println("logging_places_of_matchInfo : "+mp.getId()+" : " + mp.getPlace().getId()+"_"+mp.getPlace().getPlaceName());
-            Place place = placeService.findByIdFetchAll(mp.getPlace().getId());
+            Place place = placeRepository.findByIdFetchAll(mp.getPlace().getId()).orElseThrow(() -> new ApiException("Not Found"));
             assertEquals(mp.getPlace().getId(), place.getId());
             for(MatchPlace mp2 : place.getMatchPlaces()){
                 System.out.println("logging_place : " + place.getId()+ " : " + mp2.getId());
@@ -230,7 +261,7 @@ public class DatabaseTest {
 
         for(MatchMovie mv : matchInfo.getMovies()){
             System.out.println("logging_movies_of_matchInfo : "+mv.getId()+" : " + mv.getMovie().getId()+"_"+mv.getMovie().getMovieTitle());
-            Movie movie = movieService.findByIdFetchAll(mv.getMovie().getId());
+            Movie movie = movieRepository.findByIdFetchAll(mv.getMovie().getId()).orElseThrow(() -> new ApiException("Not Found"));
             assertEquals(mv.getMovie().getId(), movie.getId());
             for(MatchMovie mv2 : movie.getMatchMovies()){
                 System.out.println("logging_movie : " + movie.getId()+" : " + mv2.getId());
@@ -243,34 +274,34 @@ public class DatabaseTest {
         assertEquals(matchInfo.getPlaces().size(), 3);
 
 
-        /*
-            Deleting Test
-        */
 
-        matchInfoService.delete(matchInfo);
+        //    Deleting Test
 
-        List<TheDay> theDays = theDayService.findFetchAll();
+
+        matchInfoRepository.delete(matchInfo);
+
+        List<TheDay> theDays = theDayRepository.findFetchAll();
         System.out.println("TheDays_size: " + theDays.size());
         for(TheDay theDay : theDays) {
-            System.out.println("TheDay_id" + theDay.getId() + " : " + theDay.getMatchTheDays().size());
-            theDayService.delete(theDay);
+            System.out.println("TheDay_id: " + theDay.getId() + " // have " + theDay.getMatchTheDays().size() + " MatchTheDays");
+            theDayRepository.delete(theDay);
         }
 
-        List<Place> places = placeService.findFetchAll();
+        List<Place> places = placeRepository.findFetchAll();
         System.out.println("Place_size: " + places.size());
         for(Place place : places) {
-            System.out.println("Place_id" + place.getId() + " : " + place.getMatchPlaces().size());
-            placeService.delete(place);
+            System.out.println("Place_id: " + place.getId() + " // have " + place.getMatchPlaces().size() + " MatchPlaces");
+            placeRepository.delete(place);
         }
 
-        List<Movie> movies = movieService.findFetchAll();
+        List<Movie> movies = movieRepository.findFetchAll();
         System.out.println("movies_size: " + movies.size());
         for(Movie movie : movies) {
-            System.out.println("Movie_id" + movie.getId() + " : " + movie.getMatchMovies().size());
-            movieService.delete(movie);
+            System.out.println("Movie_id: " + movie.getId() + " // have " + movie.getMatchMovies().size() + " MatchMovies");
+            movieRepository.delete(movie);
         }
 
-        letterService.delete(letters.get(0));
-        letterService.delete(letters.get(1));
+        letterRepository.delete(letters.get(0));
+        letterRepository.delete(letters.get(1));
     }
 }
